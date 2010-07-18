081c9a5ba92322f8129ba19af01a46a05e0a0b9d
freepcb <freepcb@9bfb2a70-7351-0410-8a08-c5b0c01ed314>
2010-07-18 20:29:40 +0000

    Using VC++ 2005 Added Cuid class for unique identifiers Added CConnectionIterator class to iterate through connections

--- branches/allan_from_1355/NetList.cpp
+++ branches/allan_from_1355/NetList.cpp
@@ -23,9 +23,212 @@ BOOL bDontShowSelfIntersectionArcsWarning = FALSE;
 BOOL bDontShowIntersectionWarning = FALSE;
 BOOL bDontShowIntersectionArcsWarning = FALSE;
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
+/* CConnectionIterator implementation
+
+This class is sort of a "super-iterator" for moving through the
+array of connections in a net
+Unlike standard iterators, it can be used by processes which 
+add or remove connections without losing its place
+For this to work, any function that adds or removes connections has to 
+notify the iterator
+
+Rules, assuming m_net->nconnects > 0:
+	if current_ic == -1, the current connection was removed or never selected, 
+		but the position of the iterator is still maintained by prev_ic and next_ic
+	if prev_ic == -1, we are at the beginning of the connection array
+	if next_ic == -1, we are at the end of the connection array
+	if an external process removes a connection, it must call Removed(ic)
+	if an external process adds a connection, it must call Added()
+
+Example:
+	// remove any connections with more than 5 segments
+	CConnectionIterator cci( net );
+	for( int ic=cci.First(); ic=cci.Next(); ic!=-1 )
+	{
+		cconnect * c = &net->connect[ic];
+		if( c->nsegs > 5 )
+		{
+			net->connect.RemoveAt(ic);
+			cci.Removed(ic);
+		}
+	}
+*/
+
+CConnectionIterator::CConnectionIterator( cnet * net )
+{
+	if( net->m_cci )
+		ASSERT(0);	// iterator already exist, error
+	// link this iterator with net
+	m_net = net;
+	net->m_cci = this;	
+	// initialize to point to start of array, before first connection
+	// calling Next() will select the first connection
+	prev_ic = -1;
+	current_ic = -1;
+	if( m_net->nconnects )
+		next_ic = 0;
+	else
+		next_ic = -1;
+}
+
+CConnectionIterator::~CConnectionIterator()
+{
+	m_net->m_cci = NULL;	//notify net we are gone
+}
+
+int CConnectionIterator::First()
+{
+	prev_ic = -1;
+	if( m_net->nconnects == 0 )
+	{
+		current_ic = -1;
+		next_ic = -1;
+	}
+	else if( m_net->nconnects == 1 )
+	{
+		current_ic = 0;
+		next_ic = -1;
+	}
+	else
+	{
+		current_ic = 0;
+		next_ic = 1;
+	}
+	return current_ic;
+}
+
+int CConnectionIterator::Last()
+{
+	next_ic = -1;
+	if( m_net->nconnects == 0 )
+	{
+		prev_ic = -1;
+		current_ic = -1;
+	}
+	else if( m_net->nconnects == 1 )
+	{
+		current_ic = m_net->nconnects - 1;
+		prev_ic = -1;
+	}
+	else
+	{
+		current_ic = m_net->nconnects - 1;
+		prev_ic = current_ic - 1;
+	}
+	return current_ic;
+}
+
+int CConnectionIterator::Next()
+{
+	if( m_net->nconnects == 0 )
+	{
+		prev_ic = -1;
+		current_ic = -1;
+		next_ic = -1;
+	}
+	else
+	{
+		if( next_ic == -1 )
+		{
+			// we are already at end
+			current_ic = -1;
+			prev_ic = m_net->nconnects - 1;
+		}
+		else
+		{
+			// next_ic is valid, set current_ic = next_ic
+			current_ic = next_ic;
+			if( current_ic == m_net->nconnects-1 )
+				next_ic = -1;
+			else
+				next_ic = current_ic + 1;
+			if( current_ic == 0 )
+				prev_ic = -1;
+			else
+				prev_ic = current_ic - 1;
+		}
+	}
+	return current_ic;
+}
+
+int CConnectionIterator::Prev()
+{
+	if( m_net->nconnects == 0 )
+	{
+		prev_ic = -1;
+		current_ic = -1;
+		next_ic = -1;
+	}
+	else
+	{
+		if( prev_ic == -1 )
+		{
+			// we are already at start
+			current_ic = -1;
+			next_ic = 0;
+		}
+		else
+		{
+			// prev_ic is valid, set current_ic = prev_ic
+			current_ic = prev_ic;
+			if( current_ic == 0 )
+				prev_ic = -1;
+			else
+				prev_ic = current_ic - 1;
+			if( current_ic == m_net->nconnects-1 )
+				next_ic = -1;
+			else
+				next_ic = current_ic + 1;
+		}
+	}
+	return current_ic;
+}
+
+// should be called after removing a connection
+void CConnectionIterator::Removed( int ic )
+{
+	if( m_net->nconnects == 0 )
+	{
+		prev_ic = current_ic = next_ic = -1;
+	}
+	else
+	{
+		if( ic == current_ic )
+		{
+			// mark current_ic removed
+			// shift next_ic down, if possible
+			current_ic = -1;
+			if( next_ic > 0 )
+				next_ic--;
+			else
+				next_ic = -1;
+		}
+		else if( ic == next_ic )
+		{
+			// shift next_ic up if possible
+			if( next_ic < m_net->nconnects-1 )
+				next_ic++;
+			else
+				next_ic = -1;
+		}
+		else if( ic <= prev_ic )
+		{
+			// shift all down if possible
+			if( prev_ic > 0 )
+				prev_ic--;
+			else
+				prev_ic = -1;
+			if( current_ic > 0 )
+				current_ic--;
+			else
+				current_ic = -1;
+			if( next_ic > 0 )
+				current_ic--;
+			else
+				next_ic = -1;
+		}
+	}	
+}
 
 // carea constructor
 carea::carea()
--- branches/allan_from_1355/NetList.h
+++ branches/allan_from_1355/NetList.h
@@ -67,6 +67,25 @@ public:
 //
 // end definitions for ImportSessionFile()
 
+
+class CConnectionIterator
+{
+public:
+	CConnectionIterator( cnet * net );
+	~CConnectionIterator();
+	int First();
+	int Last();
+	int Next();
+	int Prev();
+	void Removed( int ic );
+	void Added();
+
+	cnet * m_net;	// net that "owns" this iterator
+	int current_ic;	// index to current connection, or -1 if current connection has been removed
+	int prev_ic;	// index to previous connection, or -1 if at beginning of array
+	int next_ic;	// index to next connection, or -1 if at end of array
+};
+
 // these structures are used for undoing 
 struct undo_pin {
 	char ref_des[MAX_REF_DES_SIZE+1];
@@ -306,6 +325,7 @@ public:
 		vtx.SetSize( 0 );
 		utility = 0;
 	};
+	~cconnect(){};
 	int start_pin, end_pin;		// indexes into net.pin array
 	int nsegs;					// # elements in seg array
 	int locked;					// 1 if locked (will not be optimized away)
@@ -339,6 +359,7 @@ public:
 	int utility;		// used to keep track of which nets have been optimized
 	int utility2;		// used to keep track of which nets have been optimized
 	CDisplayList * m_dlist;
+	CConnectionIterator * m_cci;	// connection iterator (if it exists)
 };
 
 // CNetlist
