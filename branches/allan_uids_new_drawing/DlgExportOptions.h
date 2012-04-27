0c23bd82f1aa7088d7c52f944b2fe1e36d49c69f
cptpersonal@gmail.com <cptpersonal@gmail.com@9bfb2a70-7351-0410-8a08-c5b0c01ed314>
2012-04-27 19:24:28 +0000

    Forgot to add 2 new files during r264.

---
+++ branches/allan_uids_new_drawing/DlgExportOptions.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "afxwin.h"
+
+// ALL CPT
+
+
+class CDlgExportOptions : public CDialog
+{
+	DECLARE_DYNAMIC(CDlgExportOptions)
+
+	enum { PARTS_ONLY, NETS_ONLY, PARTS_AND_NETS }; 
+	enum { FREEPCB, PADSPCB }; 
+	enum { IDD = IDD_EXPORT };
+
+public:
+	CDlgExportOptions(CWnd* pParent = NULL);   // standard constructor
+	virtual ~CDlgExportOptions();
+	void Initialize( int select );
+	int m_select;
+	int m_format;
+
+protected:
+//	virtual BOOL OnInitDialog();
+	virtual void DoDataExchange(CDataExchange* pDX);
+	CButton m_radio_parts;
+	CButton m_radio_nets;
+	CButton m_radio_parts_and_nets;
+	CButton m_radio_padspcb;
+	CButton m_radio_freepcb;
+	CButton m_check_values;
+	DECLARE_MESSAGE_MAP()
+};
+
+
